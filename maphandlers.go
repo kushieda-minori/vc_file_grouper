36a282a5030b9ee96b215cee8617d28a7e70e02e
Kushieda Minori <kushieda@zetsuboushita.net>
2015-10-28 18:05:50 -0400

    allows listing of all maps

--- maphandlers.go
+++ maphandlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 	"zetsuboushita.net/vc_file_grouper/vc"
 )
 
@@ -21,7 +22,7 @@ func mapHandler(w http.ResponseWriter, r *http.Request) {
 	pathParts := strings.Split(path[1:pathLen], "/")
 	// "maps/id"
 	if len(pathParts) < 2 {
-		http.Error(w, "Invalid map id ", http.StatusNotFound)
+		mapTableHandler(w, r)
 		return
 	}
 	mapId, err := strconv.Atoi(pathParts[1])
@@ -51,6 +52,34 @@ func mapHandler(w http.ResponseWriter, r *http.Request) {
 			e.Story,
 			e.BossStart,
 			e.BossEnd,
+		)
+	}
+	io.WriteString(w, "</tbody></table></div></body></html>")
+}
+
+func mapTableHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<html><head><title>Maps</title>\n")
+	io.WriteString(w, "<style>table, th, td {border: 1px solid black;};</style>")
+	io.WriteString(w, "</head><body>\n")
+	io.WriteString(w, "<div>\n")
+	io.WriteString(w, "<table><thead><tr>\n")
+	io.WriteString(w, "<th>Id</th><th>Name</th><th>Name Jp</th><th>Start</th><th>End</th><th>Archwitch Series</th><th>Archwitch</th><th>Elemental Hall</th><th>Flags</th><th>Beginner</th><th>Navi</th>\n")
+	io.WriteString(w, "</tr></thead>\n")
+	io.WriteString(w, "<tbody>\n")
+
+	for _, m := range VcData.Maps {
+		fmt.Fprintf(w, "<tr><td><a href=\"/maps/%[1]d\">%[1]d</a></td><td><a href=\"/maps/%[1]d\">%[2]s</a></td><td>%[3]s</td><td>%s</td><td>%s</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td>",
+			m.Id,
+			m.Name,
+			m.NameJp,
+			m.PublicStartDatetime.Format(time.RFC3339),
+			m.PublicEndDatetime.Format(time.RFC3339),
+			m.KingSeriesId,
+			m.KingId,
+			m.ElementalhallId,
+			m.Flags,
+			m.ForBeginner,
+			m.NaviId,
 		)
 	}
 	io.WriteString(w, "</tbody></table></div></body></html>")
