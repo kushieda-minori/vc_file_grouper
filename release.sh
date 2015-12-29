21ecb85f164b42d078c6a05db6c4e4aab206dd34
Kushieda Minori <kushieda@zetsuboushita.net>
2015-12-29 02:59:25 -0500

    new relase script for cross compiling binaries

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GOOS=darwin GOARCH=amd64 go build -o vc_file_grouper_OSX
+GOOS=linux GOARCH=amd64 go build -o vc_file_grouper_Linux64
+GOOS=linux GOARCH=386 go build -o vc_file_grouper_Linux32
+GOOS=windows GOARCH=amd64 go build -o vc_file_grouper_Win64
+GOOS=windows GOARCH=386 go build -o vc_file_grouper_Win32
