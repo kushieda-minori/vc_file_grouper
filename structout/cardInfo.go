ac3dd93f0f4bc705c03c8a5da1e4b0915369ec53
Kushieda Minori <kushieda@zetsuboushita.net>
2020-07-11 15:42:41 -0400

    add perfect soldier to card stat output

--- structout/cardInfo.go
+++ structout/cardInfo.go
@@ -6,22 +6,23 @@ import (
 
 // CardStatInfo card info that can be output to JSON
 type CardStatInfo struct {
-	ID            int    `json:"id"`
-	Name          string `json:"name"`
-	Element       string `json:"element"`
-	Rarity        string `json:"rarity"`
-	BaseAtk       int    `json:"baseAtk"`
-	BaseDef       int    `json:"baseDef"`
-	BaseSol       int    `json:"baseSol"`
-	MaxAtk        int    `json:"maxAtk"`
-	MaxDef        int    `json:"maxDef"`
-	MaxSol        int    `json:"maxSol"`
-	MaxLevel      int    `json:"maxLevel"`
-	MaxRarityAtk  int    `json:"maxRarityAtk"`
-	MaxRarityDef  int    `json:"maxRarityDef"`
-	MaxRaritySol  int    `json:"maxRaritySol"`
-	RebirthCardID *int   `json:"rebirthCardId"`
-	IsClosed      bool   `json:"isClosed"`
+	ID                  int    `json:"id"`
+	Name                string `json:"name"`
+	Element             string `json:"element"`
+	Rarity              string `json:"rarity"`
+	BaseAtk             int    `json:"baseAtk"`
+	BaseDef             int    `json:"baseDef"`
+	BaseSol             int    `json:"baseSol"`
+	MaxAtk              int    `json:"maxAtk"`
+	MaxDef              int    `json:"maxDef"`
+	MaxSol              int    `json:"maxSol"`
+	MaxLevel            int    `json:"maxLevel"`
+	MaxRarityAtk        int    `json:"maxRarityAtk"`
+	MaxRarityDef        int    `json:"maxRarityDef"`
+	MaxRaritySol        int    `json:"maxRaritySol"`
+	PerfectSoldierCount int    `json:"perfectSoldierCount"`
+	RebirthCardID       *int   `json:"rebirthCardId"`
+	IsClosed            bool   `json:"isClosed"`
 }
 
 // ToCardStatInfo converts the full card info to the shortened form for export
@@ -31,21 +32,22 @@ func ToCardStatInfo(c *vc.Card) CardStatInfo {
 		rebirthID = &rb.ID
 	}
 	return CardStatInfo{
-		ID:            c.ID,
-		Name:          c.Name,
-		Element:       c.Element(),
-		Rarity:        c.Rarity(),
-		BaseAtk:       c.DefaultOffense,
-		BaseDef:       c.DefaultDefense,
-		BaseSol:       c.DefaultFollower,
-		MaxAtk:        c.MaxOffense,
-		MaxDef:        c.MaxDefense,
-		MaxSol:        c.MaxFollower,
-		MaxLevel:      c.CardRarity().MaxCardLevel,
-		MaxRarityAtk:  c.CardRarity().LimtOffense,
-		MaxRarityDef:  c.CardRarity().LimtDefense,
-		MaxRaritySol:  c.CardRarity().LimtMaxFollower,
-		RebirthCardID: rebirthID,
-		IsClosed:      c.IsClosed == 1,
+		ID:                  c.ID,
+		Name:                c.Name,
+		Element:             c.Element(),
+		Rarity:              c.Rarity(),
+		BaseAtk:             c.DefaultOffense,
+		BaseDef:             c.DefaultDefense,
+		BaseSol:             c.DefaultFollower,
+		MaxAtk:              c.MaxOffense,
+		MaxDef:              c.MaxDefense,
+		MaxSol:              c.MaxFollower,
+		MaxLevel:            c.CardRarity().MaxCardLevel,
+		MaxRarityAtk:        c.CardRarity().LimtOffense,
+		MaxRarityDef:        c.CardRarity().LimtDefense,
+		MaxRaritySol:        c.CardRarity().LimtMaxFollower,
+		PerfectSoldierCount: c.EvoPerfect().Soldiers,
+		RebirthCardID:       rebirthID,
+		IsClosed:            c.IsClosed == 1,
 	}
 }
